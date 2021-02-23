7d306131ea53431e596078e113f02503e0ff3c8b
himan <you@example.com>
2021-02-23 23:22:25 +0530

    Search first commit

---
+++ Algos/src/Searching/Binary_Search.java
@@ -0,0 +1,14 @@
+package Searching;
+
+public class Binary_Search {
+	
+	// make a method for binary search
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
